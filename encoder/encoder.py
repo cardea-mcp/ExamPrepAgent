cb9831a23818a46a81c072039fc1dc276fa5db3d
ItshMoh <mohan.kumar.min22@itbhu.ac.in>
2025-07-26 22:08:40 +0530

    removed the unnecessary script and the files.

--- encoder/encoder.py
+++
@@ -1,8 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-# Load model once at the start of your application
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
-def create_embedding(text):
-    embedding = model.encode(text)
-    return embedding.tolist() 
