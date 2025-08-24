39f2334b7b73198cd3dde7828d21f112a0ff2a5e
Michael Yuan <michael@secondstate.io>
2025-08-24 04:21:29 +0000

    Change to a single tool call. Update system prompt and improve instructions in .env
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ logger = logging.getLogger(__name__)
 @mcp.tool()
 def get_random_question(topic: Optional[str] = None ):
     """
-    It is used to get a random practice question from the database
+    Get a random practice question from the database
     Arguments: It takes the topic of the question (optional).
 
     Returns:
-    string : It returns a practice or random question for the given topic
+    string : It returns a JSON structure that contains the following fields: 'question' is the practice question to be shown to the user; 'answer' is the correct answer to that question; 'explanation' is the explanation that can help the user understand the question and answer.
     """
     print("using get_random_tool")
     result = get_random_qa(topic)
@@ -25,17 +25,17 @@ def get_random_question(topic: Optional[str] = None ):
     logger.info(f"here is the random result: {result}")
     return result 
 
-@mcp.tool()
-def get_question_and_answer(question: str) -> List[Dict[str, Any]]:
-     """
-     Search for relevant question and answer pair from the database.
-     return the question and answer pair relevant to the question.
-     """ 
-     print("using get-question-tool")
-     result = search_pair(question)
-     print("result from the tool: ", result)
-     logger.info(f"here is the get question and answer result: {result}")
-     return result
+# @mcp.tool()
+# def get_question_and_answer(question: str) -> List[Dict[str, Any]]:
+#      """
+#      Search for relevant question and answer pair from the database.
+#      return the question and answer pair relevant to the question.
+#      """ 
+#      print("using get-question-tool")
+#      result = search_pair(question)
+#      print("result from the tool: ", result)
+#      logger.info(f"here is the get question and answer result: {result}")
+#      return result
 
 if __name__ == "__main__":
     print("🚀 Starting MCP server...")
